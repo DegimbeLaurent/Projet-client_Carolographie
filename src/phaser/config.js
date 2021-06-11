572b5cfbdc5ad38a3efa33119e839a182e1c9a9e
Stéphane <stephane.englebert@gmail.com>
2021-06-11 23:51:29 +0200

    example map

--- src/phaser/config.js
+++ src/phaser/config.js
@@ -1,11 +1,21 @@
 import {LoadScene} from "./scenes/LoadScene.js";
 import {AbbeyScene} from "./scenes/AbbeyScene.js";
+const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth);
+const DEFAULT_HEIGHT = window.innerHeight-20;
+const DEFAULT_WIDTH = window.innerWidth-20;
 const config = {
     type: Phaser.AUTO,
-    width: 1000,
-    height: 500,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
     backgroundColor: '#bd4545',
     parent: 'visite',
-    scene:[LoadScene, AbbeyScene]
+    scene:[LoadScene, AbbeyScene],
+    speed: 300
 };
 export { config }
--- src/phaser/scenes/AbbeyScene.js
+++ src/phaser/scenes/AbbeyScene.js
@@ -10,9 +10,17 @@ export class AbbeyScene extends Phaser.Scene{
         console.log("I GOT IT!");
     }
     preload(){
-
+        this.load.image('sprite', '/img/sprite.png');
+        this.load.image("base_tiles", "/maps/tilesheet.png");
+        this.load.tilemapTiledJSON("tilemap", "/maps/example_map2.json");
     }
     create(){
-        
+        this.scale.on('enterfullscreen', function () { });
+        const map = this.make.tilemap({ key: "tilemap" });
+        const tileset = map.addTilesetImage("tilesheet", "base_tiles");
+        const belowLayer3 = map.createLayer("ground", tileset, 0, 0);
+        const belowLayer2 = map.createLayer("grass", tileset, 0, 0);
+        const belowLayer = map.createLayer("Houses", tileset, 0, 0);
+        this.physics.add.sprite(500,500,"sprite");
     }
 }
