febe54e40bbb19584d1ce7e9ae7114ae5d427034
Stéphane <stephane.englebert@gmail.com>
2021-07-21 18:01:17 +0200

    adding Auth API

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var dotenv = require('dotenv').config();
 
 //===================================
 //   Préfixes chemins accès virtuels
